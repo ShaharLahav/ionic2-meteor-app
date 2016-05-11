833474fafa8cdb43a0084bffe46791fed51c5efc
Shahar Lahav <lahav.shahar@gmail.com>
2016-05-11 13:19:37 +0300

    Added The Server Directory

---
+++ server/lib/collections.js
@@ -0,0 +1,4 @@
+Lists = new Mongo.Collection('lists');
+ShoppingLists = new Mongo.Collection('shoppingLists');
+Userss = new Mongo.Collection('users');
+SharedLists = new Mongo.Collection('sharedLists');
